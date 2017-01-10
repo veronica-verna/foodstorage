07843f48389693ad6378fd80dcac58c370c27462
frumentum <simon-poeschl@posteo.de>
2017-01-10 14:34:27 +0100

    Add files via upload
    storage and consumption plotting work well

---
+++ prepare.R
@@ -0,0 +1,348 @@
+prepare <- function(name.of.product = "Sonnenblumenkerne",
+                    what.plotting = "compare.products",
+                    vec.of.products = kornumsatz$Produkt,
+                    vec.of.Dates  = kornumsatz$Datum,
+                    vec.of.names = lieferanten_namen,
+                    plotting.options = list(comul.change = kornumsatz$MengeKum,
+                                            food.storage = kornumsatz$Bestand_Einheit) ) {
+  
+  # for is.Date; cheeck if vec.of.Dates is a Date ####
+  require(lubridate) 
+  if (is.Date(vec.of.Dates) == FALSE) 
+    stop("Your vector of Dates has to be in format as.Date()")
+  
+  # create sortbydays ####
+  Datum <- seq(from=range(vec.of.Dates)[1], 
+               to=range(vec.of.Dates)[2], 
+               by="day") 
+  Tag_Nr <- 1:length(Datum)
+  sortbydays <- data.frame(Datum, Tag_Nr)
+  
+  
+  # starting point: create a table with just "product==x" and their equivalent position ####
+  if (is.factor(vec.of.products) == FALSE) 
+    stop("Vector of products has to be a factor")
+  
+  # cbind "Produkt" with "Position" - create sortbypos ####
+  Position <- 1:length(vec.of.products)
+  sortbypos <- data.frame(Position = Position, Produkt = vec.of.products)
+  sortbypos <- cbind(sortbypos, Datum = vec.of.Dates[sortbypos$Position])
+  
+  if (what.plotting == "allall") {
+    sortbypos <- merge(sortbypos, sortbydays, by='Datum', all=T)
+    
+    sortbypos <- cbind(sortbypos, 
+                       MengeKum = plotting.options$comul.change[sortbypos$Position],
+                       Bestand_Einheit=plotting.options$food.storage[sortbypos$Position])
+    sortbydays <- cbind(sortbydays, 
+                        MengeKum = numeric(nrow(sortbydays)),
+                        Verzehr = numeric(nrow(sortbydays)),
+                        LagerFuellen = numeric(nrow(sortbydays)), 
+                        Warenbestand = numeric(nrow(sortbydays)))
+    
+    sortbypos$MengeKum[is.na(sortbypos$MengeKum)] <- 0
+    for (i in Tag_Nr) {
+      sortbydays$MengeKum[i] <- sum(sortbypos$MengeKum[sortbypos$Datum == sortbydays$Datum[i]])
+      sortbydays$Warenbestand[i] <- sum(sortbydays$MengeKum[1:i])
+      
+      if (sortbydays$MengeKum[i] <= 0) sortbydays$Verzehr[i] <- abs(sortbydays$MengeKum[i])
+      else sortbydays$Verzehr[i] <- 0
+      
+      if (sortbydays$MengeKum[i] <= 0) sortbydays$LagerFuellen[i] <- 0
+      else sortbydays$LagerFuellen[i] <- sortbydays$MengeKum[i]
+    }
+    
+    # and here is the result
+    table <- sortbydays[, -3]
+    result <- list(data = table, name.of.product = name.of.product)
+    return(result)
+    
+  } 
+  if (what.plotting == "all.verzehr") {
+    sortbypos <- merge(sortbypos, sortbydays, by='Datum', all=T)
+    
+    sortbypos <- cbind(sortbypos, 
+                       MengeKum = plotting.options$comul.change[sortbypos$Position],
+                       Bestand_Einheit=plotting.options$food.storage[sortbypos$Position])
+    sortbydays <- cbind(sortbydays, 
+                        MengeKum = numeric(nrow(sortbydays)),
+                        Verzehr = numeric(nrow(sortbydays)),
+                        LagerFuellen = numeric(nrow(sortbydays)), 
+                        Warenbestand = numeric(nrow(sortbydays)))
+    
+    for (i in Tag_Nr) {
+      # calc sum of change for each day
+      if (is.na(sortbypos$MengeKum[sortbypos$Datum == sortbydays$Datum[i]]) == TRUE) {
+        sortbydays$MengeKum[i] <- 0
+      } else {
+        sortbydays$MengeKum[i] <- sum(sortbypos$MengeKum[which(sortbypos$Datum == sortbydays$Datum[i])]) 
+      }
+      sortbydays$Warenbestand[i] <- sum(sortbydays$MengeKum[1:i])
+      
+      if (sortbydays$MengeKum[i] <= 0) sortbydays$Verzehr[i] <- abs(sortbydays$MengeKum[i])
+      else sortbydays$Verzehr[i] <- 0
+      
+      if (sortbydays$MengeKum[i] <= 0) sortbydays$LagerFuellen[i] <- 0
+      else sortbydays$LagerFuellen[i] <- sortbydays$MengeKum[i]
+    }
+    
+    # and here is the result
+    table <- sortbydays[, -c(3,5,6)]
+    result <- list(data = table, name.of.product = name.of.product)
+    return(result)
+  }
+  if (what.plotting == "all.lager") {
+    sortbypos <- merge(sortbypos, sortbydays, by='Datum', all=T)
+    
+    sortbypos <- cbind(sortbypos, 
+                       MengeKum = plotting.options$comul.change[sortbypos$Position],
+                       Bestand_Einheit=plotting.options$food.storage[sortbypos$Position])
+    sortbydays <- cbind(sortbydays, 
+                        MengeKum = numeric(nrow(sortbydays)),
+                        Verzehr = numeric(nrow(sortbydays)),
+                        LagerFuellen = numeric(nrow(sortbydays)), 
+                        Warenbestand = numeric(nrow(sortbydays)))
+    
+    for (i in Tag_Nr) {
+      # calc sum of change for each day
+      if (is.na(sortbypos$MengeKum[sortbypos$Datum == sortbydays$Datum[i]]) == TRUE) {
+        sortbydays$MengeKum[i] <- 0
+      } else {
+        sortbydays$MengeKum[i] <- sum(sortbypos$MengeKum[which(sortbypos$Datum == sortbydays$Datum[i])]) 
+      }
+      sortbydays$Warenbestand[i] <- sum(sortbydays$MengeKum[1:i])
+      
+      if (sortbydays$MengeKum[i] <= 0) sortbydays$Verzehr[i] <- abs(sortbydays$MengeKum[i])
+      else sortbydays$Verzehr[i] <- 0
+      
+      if (sortbydays$MengeKum[i] <= 0) sortbydays$LagerFuellen[i] <- 0
+      else sortbydays$LagerFuellen[i] <- sortbydays$MengeKum[i]
+    }
+    
+    # and here is the result
+    table <- sortbydays[, -c(3,4,5)]
+    result <- list(data = table, name.of.product = name.of.product)
+    return(result)
+  }
+  else {
+    
+    if (length(name.of.product) == 1) {
+      # start here: sortbypos only for name.of.product
+      sortbypos <- sortbypos[sortbypos$Produkt == name.of.product, ] 
+      
+      # join daily change to sortbypos
+      if (what.plotting == "Verzehr") {
+        # security checks:
+        if (is.numeric(plotting.options$comul.change) == FALSE) 
+        stop("To plot consumption, at least daily change is necessary")
+        if (length(plotting.options$comul.change[sortbypos$Position]) != nrow(sortbypos)) 
+        stop("length differ! Probably you didn't specify the product for comulative change")
+        sortbypos <- cbind(sortbypos, 
+                           MengeKum = plotting.options$comul.change[sortbypos$Position])
+        
+        # merge sortbypos and sortbydays, by days of course
+        require(data.table) # v1.9.5+
+        sortbydays <- merge(sortbydays, sortbypos, by='Datum', all=T)
+        
+        # fill NAs with numbers
+        sortbydays$MengeKum[is.na(sortbydays$MengeKum)] <- 0
+        
+        # Create the consumption
+        MengeKonsum <- numeric(nrow(sortbydays))
+        for (i in sortbydays$Tag_Nr) {
+          if(sortbydays$MengeKum[i] <= 0) MengeKonsum[i] <- abs(sortbydays$MengeKum[i])
+          else MengeKonsum[i] <- 0
+        }
+        sortbydays <- cbind(sortbydays, MengeKonsum)
+        
+        # and here is the result
+        table <- data.frame(Datum = sortbydays$Datum, 
+                            Tag_Nr = sortbydays$Tag_Nr, 
+                            Verzehr = sortbydays$MengeKonsum)
+        result <- list(data = table, name.of.product = name.of.product)
+        return(result)
+      } # works
+      
+      # vectors that later on you want to plot join "sortbypos"
+      # case: plot the food storage ####
+      if (what.plotting == "Warenbestand") {
+        # security checks:
+        if (is.numeric(plotting.options$comul.change) == FALSE | 
+            is.numeric(plotting.options$food.storage) == FALSE) 
+          stop("If you want to plott food storage, you need comulative change and a vector of storage. Both same length")
+        if (length(plotting.options$comul.change) != length(vec.of.Dates) | 
+            length(plotting.options$food.storage) != length(vec.of.Dates)) 
+          stop("length differ! Probably you didn't specify the product for comulative change")
+        
+        # join comulative change and food storage to sortbypos
+        sortbypos <- cbind(sortbypos, 
+                           MengeKum = plotting.options$comul.change[sortbypos$Position], 
+                           Bestand_Einheit=plotting.options$food.storage[sortbypos$Position])
+        
+        # merge sortbypos and sortbydays, by days of course
+        require(data.table) # v1.9.5+
+        sortbydays <- merge(sortbydays, sortbypos, by='Datum', all=T)
+      
+        # fill NAs with numbers
+        sortbydays$MengeKum[is.na(sortbydays$MengeKum)] <- 0
+        for (i in 1:nrow(sortbydays)) sortbydays$Bestand_Einheit[i] <- sum(sortbydays$MengeKum[1:i])
+        
+        # and here is the result
+        table <- data.frame(Datum = sortbydays$Datum,
+                            Tag_Nr = sortbydays$Tag_Nr, 
+                            Warenbestand = sortbydays$Bestand_Einheit)
+        result <- list(data = table, name.of.product = name.of.product)
+        return(result)
+      } # works
+      
+      # food storage & consumption in one data frame
+      if (what.plotting == "all") {
+        # join comulative change and food storage to sortbypos
+        sortbypos <- cbind(sortbypos, 
+                           MengeKum = plotting.options$comul.change[sortbypos$Position],
+                           Bestand_Einheit=plotting.options$food.storage[sortbypos$Position])
+        
+        # merge sortbypos and sortbydays, by days of course
+        require(data.table) # v1.9.5+
+        sortbydays <- merge(sortbydays, sortbypos, by='Datum', all=T)
+        
+        # fill NAs with numbers
+        sortbydays$MengeKum[is.na(sortbydays$MengeKum)] <- 0
+        for (i in 1:nrow(sortbydays)) sortbydays$Bestand_Einheit[i] <- sum(sortbydays$MengeKum[1:i])
+        
+        # Create the consumption
+        MengeKonsum <- numeric(nrow(sortbydays))
+        for (i in sortbydays$Tag_Nr) {
+          if(sortbydays$MengeKum[i] <= 0) {
+            MengeKonsum[i] <- abs(sortbydays$MengeKum[i])
+          } else MengeKonsum[i] <- 0
+        }
+        sortbydays <- cbind(sortbydays, MengeKonsum)
+        
+        # and here is the result
+        table <- data.frame(Datum = sortbydays$Datum, 
+                            Tag_Nr = sortbydays$Tag_Nr, 
+                            Warenbestand = sortbydays$Bestand_Einheit,
+                            Verzehr = sortbydays$MengeKonsum)
+        result <- list(data = table, name.of.product = name.of.product)
+        return(result)
+        
+      }
+    } # one product - consumption | food.storage | both
+    
+    else {
+      
+      group_size <- length(name.of.product)
+      
+      # for each product one table
+      for (i in 1:group_size) {
+        # reset sortbypos
+        Position <- 1:length(vec.of.products) # length = 3313
+        sortbypos <- data.frame(Datum = vec.of.Dates, 
+                                Position = Position, 
+                                Produkt = vec.of.products)
+        # sortbypos <- cbind([sortbypos$Position], sortbypos)
+        
+        sortbypos <- sortbypos[sortbypos$Produkt == name.of.product[i], ]
+        # colnames(sortbypos)[1] <-  Datum (for a better overview)
+        colnames(sortbypos)[2] <- paste("Position", name.of.product[i], sep = ".")
+        colnames(sortbypos)[3] <- paste("Produkt", name.of.product[i], sep = ".")
+        # join comulative change and food storage to sortbypos
+        sortbypos <- cbind(sortbypos,
+                           MengeKum = plotting.options$comul.change[sortbypos$Position],
+                           Bestand_Einheit= plotting.options$food.storage[sortbypos$Position])
+        colnames(sortbypos)[4] <- paste("MengeKum", name.of.product[i], sep = ".")
+        colnames(sortbypos)[5] <- paste("Warenbestand", name.of.product[i], sep = ".")
+        sortbydays <- merge(sortbydays, sortbypos[,-c(2,3)], by ='Datum', all = TRUE) # without position and product column
+        
+        # define columns where NA shall be replaced by zero; c(MengeKum, LagerKilo)
+        important_columns <- c(3 + 3 * (i-1), 4 + 3 * (i-1))
+        #replace NAs with  0
+        sortbydays[,important_columns[1]][is.na(sortbydays[,important_columns[1]])] <- 0
+        
+        for (n in Tag_Nr) {
+          sortbydays[n, important_columns[2]] <- sum(sortbydays[1:n, important_columns[1]])
+        }
+        
+        # add 'sale to members'
+        MengeKonsum <- numeric(nrow(sortbydays))
+        for (t in Tag_Nr) {
+          if(sortbydays[t, important_columns[1]] <= 0) { # MengeKum.Product <= 0
+            MengeKonsum[t] <- abs(sortbydays[t, important_columns[1]])
+          } else MengeKonsum[t] <- 0
+        }
+        sortbydays <- cbind(sortbydays, MengeKonsum)
+        colnames(sortbydays)[ncol(sortbydays)] <- paste("Verzehr", name.of.product[i], sep = ".")
+      }
+        
+      LagerCol <- c()
+      VerzehrCol <- c()
+      # AuffuellenCol <- c() - doesn't work
+      for (i in 1:group_size) {
+        LagerCol[i] <- c(4 + 3 * (i-1))
+        VerzehrCol[i] <- c(5 + 3 * (i-1))
+        # AuffuellenCol[i] <- c(8 + 5 * (i-1))
+      }
+      # now build sums
+      Verzehr <- numeric(nrow(sortbydays))
+      Lager <- numeric(nrow(sortbydays))
+      for (n in Tag_Nr) {
+        Verzehr[n] <- sum(sortbydays[n, VerzehrCol])
+        Lager[n] <- sum(sortbydays[n, LagerCol])
+      }
+      sortbydays <- cbind(sortbydays, cbind(Lager, Verzehr))
+      colnames(sortbydays)[length(sortbydays) - 1] <- paste("Warenbestand", deparse(substitute(name.of.product)), sep = ".")
+      colnames(sortbydays)[length(sortbydays)] <- paste("Verzehr", deparse(substitute(name.of.product)), sep = "." )# names of product-vector
+      
+      # and here is the result
+      MengeKumCol <- c()
+      for (i in 1:group_size) {
+        MengeKumCol[i] <- c(3 + 3 * (i-1))
+      }
+      remove <- sort(MengeKumCol)
+      table <- sortbydays[, -remove]
+      
+      if (what.plotting == "all") {
+        result <- list(data = table, name.of.product = name.of.product, name.of.group = deparse(substitute(name.of.product)))
+        return(result)
+      } 
+      
+      if (what.plotting == "compare.products") {
+        table <- table[, c(1,2,ncol(table) - 1, ncol(table))]
+        result <- list(data = table, name.of.product = name.of.product, name.of.group = deparse(substitute(name.of.product)))
+        return(result)
+      }
+      
+      if (what.plotting == "Verzehr") {
+        table <- table[, c(1,2,ncol(table))]
+        colnames(table)[3] <- "Verzehr"
+        result <- list(data = table, name.of.product = name.of.product, name.of.group = deparse(substitute(name.of.product)))
+        return(result)
+      }
+      
+      if (what.plotting == "Warenbestand") {
+        table <- table[, c(1,2,ncol(table)-1)]
+        colnames(table)[3] <- "Warenbestand"
+        result <- list(data = table, name.of.product = name.of.product, name.of.group = deparse(substitute(name.of.product)))
+        return(result)
+      }
+      
+    } # compare groups
+      
+      # add buying food - doesn't work
+      #LagerFuellen <- numeric(nrow(sortbydays))
+      #for (t in sortbydays$Tag_Nr) {
+      #if (sortbydays[t, important_columns[2]] <= 0) {
+      # LagerFuellen[t] <- 0
+      #} else LagerFuellen[t] <- sortbydays[t, important_columns[2]]
+      #}
+      
+      # add both to sortbydays
+      #sortbydays <- cbind(sortbydays, MengeKonsum, LagerFuellen)
+      #colnames(sortbydays)[c(ncol(sortbydays) - 1, ncol(sortbydays))] <- c(paste("Verzehr", name.of.product[i], sep = "."), paste("Aufgefuellt", name.of.product[i], sep = "."))
+      
+      #}
+    
+  } 
+
+}
---
+++ start_up_settings.R
@@ -0,0 +1,60 @@
+# Sortieren der Daten, Hinzufügen von Spalten, etc. ####
+kornumsatz <- read.csv("~/Documents/Studium/Bachelor-Arbeit/R-paket/source/kornumsatz.csv", sep=";")
+Position <- 1:nrow(kornumsatz)
+kornumsatz <- cbind(Position, kornumsatz)
+kornumsatz$Datum <- as.Date(kornumsatz$Datum, format="%d/%m/%Y")
+
+# Laden der zwei wichtigen Funktionen
+source(prepare.R)
+source(myPlot.R)
+
+# plotten eines beliebigen Produkts
+myPlot(prepare("Dinkel", what.plotting = "Verzehr"))
+
+# Plot Funktion hat noch viele grafische Parameter wie col_line, oder lty, lwd, col_points ....
+
+### verschiedene Gruppen ####
+Linsen.Braun <- c("Linsen Braun", "Braune Linsen", "Tellerlinsen")
+
+Huelsenfruechte <- c("Beluglinsen", "Bohnen", "Bohnen Borlotti", "Borlottibohnen", "Braune Linsen", "Linsen Beluga", "Linsen Braun", "Rote Linsen", "Rote Linsen Neu", "Tellerlinsen")
+
+Luzernenhof <- c("Dinkel", "Hafer", "Roggen", "Weizen")
+BioHofLex <- c("Beluglinsen", "Bohnen Borlotti", "Borlottibohnen", "Braune Linsen", "Braunhirse", "Buchweizen", "Hirse Braun", "Linsen Beluga", "Linsen Braun", "Polenta", "Tellerlinsen")
+OBEG <- c("Apfelessig", "Haferflocken", "Kürbiskerne", "Langkornreis Natur", "Leinsamen", "Rapsöl", "Rübenzucker Sack", "Rundkornreis", "Salz", "Sesam", "Sonnenblumenkerne", "Sonnenblumenöl") # unsicher bei Kichererbsen 2. Ladung, Rundkornreis, Sesam, Sonnenblumenkerne
+HakumaFood <- c("Senf Kirsche", "Senf Sarepta", "Senf Mango")
+HofgutStorzeln <- c("Dinkel Drink", "Drink Buchweizen", "Drink Dinkel", "Drink Hafer", "Drink Soja", "Hafer Drink", "Soja Drink")
+HofgutUnterbach <- c("Haselnüsse geschält", "Walnüsse")
+Erich <- c("Honig", "Sonnenblütenhonig")
+Cashew4you <- c("Cashews")
+Sonett <- as.character(c("Allesreiniger", "Spülmittel", "Spülmittel Hand", "Spülmittel Maschine", "Waschmittel Lavendel", "Waschmittel Normal", "Waschmittel Pulver", "Waschmittel Seide/Wolle", "Waschmittel Sensitiv"))
+ElephantBeans <- c("Kaffee Espresso", "Kaffee Geröstet", "Kaffe Espresso", "Kaffee Zumba", "Kaffe Zumba")
+Sonnenobst <- c("Getrocknete Äpfel", "Getrocknete Paprika", "Getrocknetes Gemüse")
+Naturata <- c("Gemüsebrühe", "Kokosfett", "Olivenöl", "Penne semoltano", "Rübenzucker", "Spaghetti", "Spaghetti semoltano", "Spirelli", "Tomatenmark", "Tomaten Mark", "Tomatenpassata", "Tomaten Passata")
+Wolfgang <- c("Bohnen", "Saft Apfel", "Saft Apfel-Birne", "Saft Apfel-Möhre", "Saft Trauben")
+Großhandel <- c("Arborio Reis", "Back-/Bratöl, EU", "Basilikum", "Basitom", "Basmati Braun", "Basmati Weiß", "Blaumohn", "brauner Basmati", "Buchweizenmehl", "Couscous", "Currychini", "Erdnussmus", "Goldhirse", "Goldhirse Neu", "Grünkern", "Kichererbsen", "Kräuter der Provence", "Kümmel", "Mandelmus", "Mepfel", "Oregano", "Paprika Edelsüß", "Risottoreis", "Rohrohrzucker", "Rosinen", "Rosmarin", "Rote Beete Meerettich", "Rote Linsen", "Rote Linsen Neu", "Samba", "Samba - Schokoaufstrich", "Schwarzkümmel", "Sendi", "Senfkörner", "Senfkörner Neu", "Sonnenblumenöl, EU ", "Thymian", "Weinbeeren", "weißer Basmati", "Zimt Gemahlen", "Zwiebelschmelz") # SB-Kerne + Zimt Gemahlen, evtl Sesam
+spanischeKoop <- as.character(c("Olivenöl"))
+NepalVerein <- c("Bockshornklee Ganz", "Chilli Gemahlen", "Ingwer Gemahlen", "Kaffee roh", "Kardamom Ganz", "Koriander Ganz", "Koriander Gemahlen", "Kreuzkümmel Ganz", "Kreuzkümmel Gemahlen", "Kurkuma Gemahlen", "Pfeffer Schwarz Ganz", "Senfkörner Ganz", "Zimt Ganz", "Zimt Gemahlen")
+
+lieferanten <- list(BioHofLex, Erich, Luzernenhof, OBEG, HakumaFood, HofgutStorzeln, HofgutUnterbach, Sonett, ElephantBeans, Naturata, Sonnenobst, Wolfgang, Großhandel, NepalVerein)
+
+lieferanten_namen <- c(deparse(substitute(BioHofLex)), deparse(substitute(Erich)), deparse(substitute(Luzernenhof)), deparse(substitute(OBEG)), deparse(substitute(HakumaFood)), deparse(substitute(HofgutStorzeln)), deparse(substitute(HofgutUnterbach)), deparse(substitute(Sonett)), deparse(substitute(ElephantBeans)), deparse(substitute(Naturata)), deparse(substitute(Sonnenobst)), deparse(substitute(Wolfgang)), deparse(substitute(Großhandel)), deparse(substitute(NepalVerein)), deparse(substitute(Cashew4you)), deparse(substitute(spanischeKoop)))
+verzehr_sum <- numeric()
+lager_sum <- numeric()
+for (i in 1:length(lieferanten)) {
+  verzehr_sum[i] <- sum(prepare(lieferanten[[i]])$data[,4])
+  lager_sum[i] <- sum(prepare(lieferanten[[i]])$data[,3])
+}
+verzehr_sum[15] <- sum(prepare("Cashews", "all")$data[,4])
+verzehr_sum[16] <- sum(prepare("Olivenöl", "all")$data[,4])
+lager_sum[15] <- sum(prepare("Cashews", "all")$data[,3])
+lager_sum[16] <- sum(prepare("Olivenöl", "all")$data[,4])
+
+
+### nur Großhandel und direkt Produzenten ####
+par(mar=c(8,5, 4, 2))
+Großhandel <- sum(as.numeric(verzehr_sum[c(8,10,13)])) # Bode, Daver, Naturata, Sonett
+DirektErzeuger <- sum(as.numeric(verzehr_sum[-c(8,10,13)])) # alles andere
+barplot(c(Großhandel, DirektErzeuger), names.arg = c("Großhandel", "Direkt"), ylab="Verzehr in Kilo", main = "Verzehr nach Lieferanten geordnet", las = 2, ylim = c(0,2000))
+legend("topleft", legend=c("Großhandel bestehend aus", "Sonett", "Naturata", "Bode", "Davert"), fill = c("white", "black", "black", "black", "black"))
+barplot(verzehr_sum, names.arg = lieferanten_namen, ylab = "Verzehr in Kilo", main = "Verzehr nach Lieferanten geordnet", las = 2)
+
