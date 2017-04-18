72cadfa667188a5ab82aee18702afd26467f5a3b
frumentum <frumentum@posteo.de>
2017-04-18 16:25:29 +0200

    some changing in default displaying 'Warenbestand'

--- R/myPlot.R
+++ R/myPlot.R
@@ -11,10 +11,13 @@ myPlot <- function(prepare,
                    lty = c(1,2),
                    axis_side = c(2,4),
                    las = c(1,1),
-                   line = c(2,2),
+                   line = c(3,3),
                    mfrow = c(1,1),
-                   mar = c(2,4,2,4)+0.1,
-                   add = FALSE){
+                   mar = c(4, 5, 4, 1) + 0.1,
+                   add = FALSE,
+                   cex.axis = 1.3,
+                   cex.lab = 1.6,
+                   cex.main = 1.6){
   
   # at first: check if the input is correct ####
     if (is.list(prepare) != TRUE) stop("prepare has to be a list: first element $data is a data frame, second element $name.of.product is a character string")
@@ -52,7 +55,8 @@ myPlot <- function(prepare,
            ylim = c(range(preds)[1] - 0.2, range(preds)[2] + 0.2), 
            pch = pch[1], 
            main = paste(paste(colnames(data)[3], "von"), name.of.product), 
-           col = col_points[1])
+           col = col_points[1],
+           cex.main = cex.main)
       } else {
         if (add == FALSE) par(mfrow = mfrow, mar = mar) 
         plot(x = Datum, 
@@ -64,15 +68,16 @@ myPlot <- function(prepare,
              ylim = c(range(preds)[1] - 0.2, range(preds)[2] + 0.2), 
              pch = pch[1], 
              main = paste(paste(colnames(data)[3], "von"), prod.name.for.plot), 
-             col = col_points[1])
+             col = col_points[1],
+             cex.main = cex.main)
       } # for lazy people filled prod.name.for.plot is not necessary!
       
       # now lines are drawed
       lines(x = Datum, y = preds, col = col_line[1], lwd = lwd[1], lty = lty[1])
       
       # and the y-axis gets a scription
-      axis(side = axis_side[1], las = las[1])
-      mtext(text = paste(colnames(data)[3], "in Kilo"), side = axis_side[1], line = line[1])
+      axis(side = axis_side[1], las = las[1], cex.axis = cex.axis)
+      mtext(text = paste(colnames(data)[3], "in Kilo"), side = axis_side[1], line = line[1], cex = cex.lab)
     
   } # normally necessary
   
@@ -87,7 +92,7 @@ myPlot <- function(prepare,
     }
     
     # build an empty plot
-    par(mfrow=c(1,1), mar=c(2,4,2,4)+0.1)
+    par(mfrow=c(1,1), mar=c(2,6,2,6)+0.1)
     
     if (prod.name.for.plot == "") {
       warning("Main will look better if you give the product's name in prod.name.for.plot")
@@ -101,7 +106,8 @@ myPlot <- function(prepare,
            ylim = c(range(preds_waren)[1] - 0.2, range(preds_waren)[2] + 0.2), 
            pch = pch[1], 
            main = paste("Warenbestand & Verzehr von", name.of.product), 
-           col = col_points[1])
+           col = col_points[1],
+           cex.main = cex.main)
     } else {
       if (add == FALSE) par(mfrow = mfrow, mar = mar) 
       plot(x = Datum, 
@@ -113,15 +119,16 @@ myPlot <- function(prepare,
            ylim = c(range(preds_waren)[1] - 0.2, range(preds_waren)[2] + 0.2), 
            pch = pch[1], 
            main = paste("Warenbestand & Verzehr von", prod.name.for.plot), 
-           col = col_points[1])
+           col = col_points[1],
+           cex.main = cex.main)
     } # for lazy people filled prod.name.for.plot is not necessary!
     
     # now storage is getting drawed
     lines(x = Datum, y = preds_waren, col = col_line[1], lwd = lwd[1], lty = lty[1])
     
     # (left) y-axis gets its scription
-    axis(side = axis_side[1], las = las[1])
-    mtext(text = "Warenbestand in Kilo", side = axis_side[1], line = line[1])
+    axis(side = axis_side[1], las = las[1], cex.axis = cex.axis)
+    mtext(text = "Warenbestand in Kilo", side = axis_side[1], line = line[1], cex = cex.lab)
     
     # consumption is added
     par(new=TRUE)
@@ -144,8 +151,8 @@ myPlot <- function(prepare,
     lines(x = Datum, y = preds_verzehr, col = col_line[2], lwd = lwd[2], lty = lty[2])
     
     # (right) y-axis gets its scription
-    axis(side = axis_side[2], las = las[2])
-    mtext(text = "Verzehr in Kilo", side = axis_side[2], line = line[2])
+    axis(side = axis_side[2], las = las[2], cex.axis = cex.axis)
+    mtext(text = "Verzehr in Kilo", side = axis_side[2], line = line[2], cex = cex.lab)
     
     legend("topleft", lty=c(lty[1], lty[2]), legend=c("Warenbestand", "Verzehr"), lwd=c(lwd[1], lwd[2]), col=c(col_line[1], col_line[2]))
   } # only needed if consumption and storage shall be plotted in one plot
@@ -161,6 +168,6 @@ myPlot <- function(prepare,
   end_date <- max(Datum)
   date1st <- seq(from = as.Date(start_1st_date), to = end_date, by = '1 month')
   # do the writing!
-  axis(1, at=date1st, labels=month.abb[month(as.POSIXlt(date1st, format="%Y-%m-%d"))]) # as.posix gives the month in number, month.abb changes the month' number in the first 3 letters; month would give the whole month' name    
+  axis(1, at=date1st, labels=month.abb[month(as.POSIXlt(date1st, format="%Y-%m-%d"))], cex.axis = cex.axis) # as.posix gives the month in number, month.abb changes the month' number in the first 3 letters; month would give the whole month' name    
       
 }
