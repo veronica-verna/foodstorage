519e9be0cf3cc52e2d2347eee8bbc7cfdab2dde4
frumentum <frumentum@posteo.de>
2017-09-16 02:51:05 +0200

    start correction and eleminating empty products

--- R/currentStorage.R
+++ R/currentStorage.R
@@ -8,6 +8,8 @@ currentStorage <- function(group, plot = TRUE, horiz = FALSE, fill = TRUE) {
   # create ordered factor
   big.df$Produkt <- factor(big.df$Produkt, levels = big.df$Produkt)
   
+  # make correction
+  
   # coloured bars or black-white: create ggplot object
   if (fill == TRUE) {
     plotcur <- ggplot(big.df, aes(x = factor(Produkt), y = Bestand_Einheit, fill = factor(Produkt))) +
@@ -23,13 +25,14 @@ currentStorage <- function(group, plot = TRUE, horiz = FALSE, fill = TRUE) {
       labs(title = "Aktueller Warenbestand", x= "Produkte", y = "Warenbestand in Kilo") + 
       # theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + 
       coord_flip() +
-      guides(fill = FALSE) 
+      guides(fill = FALSE)
   } else {
     plotcur + 
       geom_col() + 
       labs(title = "Aktueller Warenbestand", x= "Produkte", y = "Warenbestand in Kilo") + 
       theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + 
       guides(fill = FALSE) +
-      coord_cartesian(ylim = c(0,max(big.df$Bestand_Einheit))) 
+      coord_cartesian(ylim = c(0,max(big.df$Bestand_Einheit))) +
+      guid
   }
 }
