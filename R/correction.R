fbc685dc85c4e660b0ac203a3c58c6c061ae1f52
frumentum <frumentum@posteo.de>
2017-10-09 11:32:43 +0200

    try re-understanding of correction...

--- R/correction.R
+++ R/correction.R
@@ -9,7 +9,7 @@ correction <- function(sub.df,
                        test_candidates = FALSE,
                        test_equal_candidates = FALSE) {
   
-  if (findInterval(correction, c(0,0.2)) == 0) stop("A correction for more than twenty percent isn't realistic")
+  if (findInterval(correction, c(0,0.2)) != 1) stop("A correction for more than twenty percent isn't realistic")
   empty <- data.frame() # an empty data.frame for containing empty 'storages'
   Prozent5 <- correction * VPE
   sub.df <- cbind(sub.df, Prozent5)
@@ -17,13 +17,13 @@ correction <- function(sub.df,
   candidates <- sub.df[sub.df$Bestand_Einheit < sub.df$Prozent5,]
   if (isTRUE(test_candidates)) return(candidates) # for debugging with test_that
   
-  # comparison...
+  # compare candidates with the next entry/row of sub.df and check their dates
   candidates_eval <- sub.df[which(sub.df$Position %in% candidates$Position)+1, ]
-  if (TRUE %in% is.na(candidates_eval$Position)) {
-    candidates_eval <- candidates_eval[which(!is.na(candidates_eval$Position)),] 
-    empty <- candidates[nrow(candidates), ]
-    candidates <- candidates[-nrow(candidates),] # clean ...
-  }
+  # if (TRUE %in% is.na(candidates_eval$Position)) {
+  #   candidates_eval <- candidates_eval[which(!is.na(candidates_eval$Position)),] 
+  #   empty <- candidates[nrow(candidates), ]
+  #   candidates <- candidates[-nrow(candidates),] # clean ...
+  # }
   if (isTRUE(test_equal_candidates)) return(nrow(candidates) - nrow(candidates_eval))
   # date_dif <- 
 }
