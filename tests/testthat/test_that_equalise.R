02d395036f82cb711a799c42b075d3fc5a2b7219
frumentum <frumentum@posteo.de>
2017-11-25 23:58:09 +0100

    new starting_csv added + one more test

---
+++ tests/testthat/test_that_equalise.R
@@ -0,0 +1,27 @@
+# test equalise
+test_korn1 <- data.frame(
+  Produkt = factor(c("Buchweizen", "Dinkel", "Hafer", "Weizen"))
+)
+test_start <- data.frame(
+  Produkt = factor(c("Buchweizen", "Dinkel", "Hafer", "Weizen"))
+)
+test_korn2 <- data.frame(
+  Produkt = factor(c("Buchweizen", "Cashews", "Dinkel", "Hafer", "Weizen"))
+)
+
+
+test_that(
+  "all products are known",
+  {
+    len <- length(equalise(data = test_korn1, startingCSV = test_start))
+    expect_equal(len, 0)
+  }
+)
+
+test_that(
+  "all products are known",
+  {
+    len <- length(equalise(data = test_korn1, startingCSV = test_start))
+    expect_equal(len, 0)
+  }
+)
