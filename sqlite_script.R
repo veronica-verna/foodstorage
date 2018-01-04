de5c27d7263625993390218d271c09a9a5258c85
frumentum <frumentum@posteo.de>
2018-01-04 17:43:54 +0100

    built sqlite infrastructure for data handling - but still in a script

--- sqlite_script.R
+++ sqlite_script.R
@@ -1,7 +1,10 @@
 # start trial: use RSQLITE
 library(DBI)
-mydb <- dbConnect(SQLite(), paste0("data/", list.files("data")[2])) # connect to trial backup
-kornumsatz_origin <- dbGetQuery(mydb, '
+orig <- dbConnect(SQLite(), "data/Backup_2017_09_19_origin.BAK")
+dbListTables(orig)
+
+appDB <- dbConnect(SQLite(), paste0("data/", list.files("data")[1])) # connect to trial backup
+kornumsatz_origin <- dbGetQuery(appDB, '
 SELECT strftime(\'%d/%m/%Y\',transactions.start/1000,\'unixepoch\') AS Tag,
       ROUND(SUM(transaction_products.quantity), 3) AS Menge, 
       transaction_products.unit AS Einheit,
@@ -26,16 +29,15 @@ kornumsatz_origin <- kornumsatz_origin %>%
   arrange(Tag) %>%
   mutate(Tag = as.character(Tag))
 
+### create new data base
+mydb <- dbConnect(SQLite(), "data/kornInfo.sqlite")
+
 dbWriteTable(
   mydb,
   "kornumsatz_origin",
   kornumsatz_origin,
   overwrite = T
 )
-
-# kornumsatz_origin <- dbReadTable(mydb, "kornumsatz_origin") %>%
-#   mutate(Tag = as.Date(Tag, format = "%Y-%m-%d"))
-
 
 dbWriteTable(
   mydb, 
@@ -46,8 +48,11 @@ dbWriteTable(
 dbListTables(mydb)
 library(foodstorage)
 
+kornumsatz_origin <- dbReadTable(mydb, "kornumsatz_origin") %>%
+  mutate(Tag = as.Date(Tag, format = "%Y-%m-%d"))
+
 kornumsatz_edit <- foodstorage::startup.settings(
-  dbReadTable(mydb, "kornumsatz_origin"),
+  kornumsatz_origin,
   dbReadTable(mydb, "product_info"), 
   reduce = T
 )
@@ -60,12 +65,40 @@ dbWriteTable(
 )
 
 # test
-dat <- dbReadTable(mydb, "kornumsatz_origin") %>%
-  filter(Produkt == "Olivenöl") %>%
-  mutate(cumsum = cumsum(Menge)) %>%
-  mutate(Tag = as.Date(Tag, format = "%Y-%m-%d")) %>%
-  select(Tag, Bestand.Einheit, cumsum)
+library(testthat)
+test_that(
+  "cumulative sum for every product works", {
+    dat <- dbReadTable(mydb, "kornumsatz_origin") %>%
+      filter(Produkt == "Olivenöl") %>%
+      mutate(cumsum = cumsum(Menge)) %>%
+      mutate(Tag = as.Date(Tag, format = "%Y-%m-%d")) %>%
+      select(Tag, Bestand.Einheit, cumsum)
+    
+    expect_equal(
+      round(dat[,2] - dat[,3], 3),
+      rep(0,nrow(dat))
+    )
+  }
+)
 
-round(p2-p1, 3) # Test ob es funktioniert -> ist gleich
-ggplot(dat, aes(x = Tag, y = Bestand.Einheit)) +
-  geom_line()
+
+### bank transactions
+bank <- dbGetQuery(orig, "
+  SELECT quantity, 
+        strftime(\'%d/%m/%Y\',start/1000,\'unixepoch\') AS Tag, 
+        comment
+  FROM transaction_products
+  JOIN transactions ON transaction_products.transaction_id = transactions._id
+  WHERE account_guid IS 'bank'
+") %>%  
+  mutate(Tag = as.Date(Tag, format = "%d/%m/%Y")) %>% 
+  arrange(Tag)
+
+bank2016 <- bank %>%
+  filter(year(Tag) == 2016)
+
+bank <- dbGetQuery(orig, "
+  SELECT * 
+  FROM transaction_products
+  WHERE account_guid IS 'bank'
+")
